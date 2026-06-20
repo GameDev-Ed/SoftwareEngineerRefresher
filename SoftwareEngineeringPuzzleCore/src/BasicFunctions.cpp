24e3892f8e8b0c91f3d130711284a46e87646003
Edward Spellman <edwardspellman.swe@gmail.com>
2026-06-19 23:07:31 -0400

    Finished the BasicFunctions File. It was pretty simple, but the GetLetterGrade even though I used only if/else if, I could have used a switch case ( put it in there) but for the sense of juist refreshing my mind I did the ifs.

--- SoftwareEngineeringPuzzleCore/src/BasicFunctions.cpp
+++ SoftwareEngineeringPuzzleCore/src/BasicFunctions.cpp
@@ -2,35 +2,94 @@
 
 namespace basics
 {
-    int Add(int a, int b)
-    {
-        // TODO PUZZLE 1:
-        // Software engineering idea: functions should do one clear job.
-        // Replace this placeholder with code that returns the sum of a and b.
-        return 0;
-    }
+	int Add(int a, int b)
+	{
+		// TODO PUZZLE 1:
+		// Software engineering idea: functions should do one clear job.
+		int result = a + b;
+		return result;
+	}
 
-    bool IsAdult(int age)
-    {
-        // TODO PUZZLE 2:
-        // Software engineering idea: conditions let your program make decisions.
-        // Return true when age is 18 or above. Otherwise return false.
-        return false;
-    }
+	bool IsAdult(int age)
+	{
+		// TODO PUZZLE 2:
+		// Software engineering idea: conditions let your program make decisions.
+		// Return true when age is 18 or above. Otherwise return false.
+		if (age >= 18)
+		{
+			return true;
+		}
+		else
+			return false;
+	}
 
-    char GetLetterGrade(int score)
-    {
-        // TODO PUZZLE 3:
-        // Software engineering idea: order matters when checking ranges.
-        // Example: 95 should return 'A', not 'B'.
-        return '?';
-    }
+	char GetLetterGrade(int score)
+	{
+		// TODO PUZZLE 3:
+		// Software engineering idea: order matters when checking ranges.
+		// Example: 95 should return 'A', not 'B'.
 
-    bool IsValidUsername(const std::string& username)
-    {
-        // TODO PUZZLE 4:
-        // Software engineering idea: validation protects systems from bad input.
-        // Return true only when username has at least 3 characters.
-        return false;
-    }
+		if (score >= 90)
+		{
+			return 'A';
+		}
+		else if (score >= 80)
+		{
+			return 'B';
+		}
+		else if (score >= 70)
+		{
+			return 'C';
+
+		}
+		else if (score >= 60)
+		{
+			return 'D';
+		}
+		else
+			return 'F';
+		/*
+		The reason case 10 and case 9 both return 'A' is because:
+
+			100 / 10 = 10
+			95 / 10 = 9
+			90 / 10 = 9
+		switch (score / 10)
+	{
+	case 10:
+	case 9:
+		return 'A';
+
+	case 8:
+		return 'B';
+
+	case 7:
+		return 'C';
+
+	case 6:
+		return 'D';
+
+	default:
+		return 'F';
+	}
+		*/
+
+
+
+
+
+	}
+
+	bool IsValidUsername(const std::string& username)
+	{
+		// TODO PUZZLE 4:
+		// Software engineering idea: validation protects systems from bad input.
+		// Return true only when username has at least 3 characters.
+		if (username.size() >= 3)
+		{
+			return true;
+		}
+		else
+			return false;
+	}
 }
